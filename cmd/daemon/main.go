c9506d8c25ede3eaeca835176e57e2a4cdd30eb7
ManuGH <85080715+ManuGH@users.noreply.github.com>
2025-09-26 04:22:50 +0200

    main.go

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,1 +1,45 @@
-1
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/ManuGH/xg2g/internal/api"
+	"github.com/ManuGH/xg2g/internal/jobs"
+)
+
+func main() {
+	cfg := jobs.Config{
+		DataDir:   env("XG2G_DATA", "/data"),
+		OWIBase:   env("XG2G_OWI_BASE", "http://10.10.55.57"),
+		Bouquet:   env("XG2G_BOUQUET", "Premium"),
+		PiconBase: env("XG2G_PICON_BASE", ""),
+		XMLTVPath: env("XG2G_XMLTV", ""),
+		FuzzyMax:  atoi(env("XG2G_FUZZY_MAX", "2")),
+	}
+
+	s := api.New(cfg)
+	addr := env("XG2G_LISTEN", ":34400")
+	
+	log.Printf("Starting xg2g on %s", addr)
+	log.Printf("Config: data=%s, owi=%s, bouquet=%s, xmltv=%s, fuzzy=%d", 
+		cfg.DataDir, cfg.OWIBase, cfg.Bouquet, cfg.XMLTVPath, cfg.FuzzyMax)
+		
+	log.Fatal(http.ListenAndServe(addr, s.Handler()))
+}
+
+func env(k, def string) string {
+	if v := os.Getenv(k); v != "" {
+		return v
+	}
+	return def
+}
+
+func atoi(s string) int {
+	if v, err := strconv.Atoi(s); err == nil {
+		return v
+	}
+	return 0
+}
