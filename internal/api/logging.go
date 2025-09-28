7ed555391fb3e549c131a35ba45ce5ac746052a0
ManuGH <85080715+ManuGH@users.noreply.github.com>
2025-09-28 19:57:27 +0200

    refactor(api): reuse central logger factory

--- internal/api/logging.go
+++ internal/api/logging.go
@@ -1,33 +1,12 @@
 package api
 
 import (
-	"os"
-	"time"
-
+	xglog "github.com/ManuGH/xg2g/internal/log"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
-func init() {
-	// Configure zerolog
-	zerolog.TimeFieldFormat = time.RFC3339
-	// Set global log level from environment if provided
-	if lvlStr := os.Getenv("LOG_LEVEL"); lvlStr != "" {
-		if lvl, err := zerolog.ParseLevel(lvlStr); err == nil {
-			zerolog.SetGlobalLevel(lvl)
-		}
-	}
-	log.Logger = log.Output(zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: time.RFC3339,
-	})
-}
-
-// Logger returns a context-aware logger configured with component metadata
+// logger returns a context-aware logger configured with component metadata.
 func logger(component string) *zerolog.Logger {
-	logger := log.With().
-		Str("component", component).
-		Str("version", os.Getenv("VERSION")).
-		Logger()
-	return &logger
+	l := xglog.WithComponent(component)
+	return &l
 }
